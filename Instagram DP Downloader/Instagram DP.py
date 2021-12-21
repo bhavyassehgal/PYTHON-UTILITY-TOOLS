8fbed1a0c8ccd7ee17d9bd18f60eca4655b09eab
Bhavya Sehgal <85365197+bhavyassehgal@users.noreply.github.com>
2021-12-21 20:24:35 +0530

    Python Code for Instagram DP Downloader

---
+++ Instagram DP Downloader/Instagram DP.py
@@ -0,0 +1,9 @@
+# importing libraries
+import instaloader
+
+# using libraries
+mod=instaloader.Instaloader()
+a=input("Enter The Profile Name --> ")
+mod.download_profile(a,profile_pic_only=True)
+
+#CODE BY: BHAVYA SEHGAL
